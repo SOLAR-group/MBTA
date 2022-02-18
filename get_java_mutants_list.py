4866dd60bc312ede757528f63e88a74d3897e4e7
Giovani Guizzo <giovaniguizzo@gmail.com>
2022-02-18 12:07:09 +0000

    Added the list of java classes and mutants

---
+++ get_java_mutants_list.py
@@ -0,0 +1,19 @@
+import os.path
+from pathlib import Path
+
+from glob2 import glob
+
+result_path = os.path.join("..", "MuJava", "result")
+containing_dirs = glob(result_path + "/*")
+dir_names = [Path(directory).name for directory in containing_dirs]
+with open("java_classes.txt", "w") as classes_file:
+    [classes_file.write(clazz + "\n") for clazz in dir_names]
+
+with open("java_mutants.txt", "w") as mutants_file:
+    for containing_dir in containing_dirs:
+        mutants_dir = os.path.join(containing_dir, "traditional_mutants")
+        with open(os.path.join(mutants_dir, "method_list"), "r") as method_list:
+            method_name = method_list.readline().strip("\n")
+        mutants_dir = os.path.join(mutants_dir, method_name)
+        mutants_list = glob(mutants_dir + "/*/*.java")
+        [mutants_file.write(str(mutant).replace(result_path, "").replace("\\", "/").strip("/") + "\n") for mutant in mutants_list]
